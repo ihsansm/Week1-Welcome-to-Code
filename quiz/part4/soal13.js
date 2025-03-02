d6499c26196722926cf1264975485f85213b3840
ihsan <ihsantwf301@gmail.com>
2025-03-03 05:26:53 +0700

    Submit part4

---
+++ quiz/part4/soal13.js
@@ -0,0 +1,12 @@
+// variable input
+let num1 = 1
+let num2 = 2
+
+// function: 
+function calculateMultiply(num1, num2){
+  return num1 * num2;
+}
+ 
+// variable hasil
+let hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian); 
