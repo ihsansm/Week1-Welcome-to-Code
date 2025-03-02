66901a35527d0f98a1c41aaeba245432fb35c1bc
ihsan <ihsantwf301@gmail.com>
2025-03-02 22:27:17 +0700

    Submit soal5

---
+++ quiz/part2/soal5.js
@@ -0,0 +1,13 @@
+let word3 = 'wow JavaScript is so cool';
+let FirstWord3 = word3.substring(0, 3);
+let SecondWord3 = word3.substring(4, 14);
+let ThirdWord3 = word3.substring(15, 17);
+let FourthWord3 = word3.substring(18, 20);
+let FifthWord3 = word3.substring(21, 25);
+
+// output
+console.log('First Word: ' + FirstWord3);
+console.log('Second Word: ' + SecondWord3);
+console.log('Third Word: ' + ThirdWord3);
+console.log('Fourth Word: ' + FourthWord3);
+console.log('Fifth Word: ' + FifthWord3);
