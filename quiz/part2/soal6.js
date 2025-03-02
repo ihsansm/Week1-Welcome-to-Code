996eb4859695078c12e4950d9e0d1131ded30fee
ihsan <ihsantwf301@gmail.com>
2025-03-02 22:36:52 +0700

    Submit soal6

---
+++ quiz/part2/soal6.js
@@ -0,0 +1,19 @@
+let word4 = 'wow JavaScript is so cool';
+let FirstWord4 = word4.substring(0, 3);
+let SecondtWord4 = word4.substring(4, 14);
+let ThirdWord4 = word4.substring(15, 17);
+let FourthWord4 = word4.substring(18, 20);
+let FifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = FirstWord4.length;
+let SecondtWordLength = SecondtWord4.length;
+let ThirdWordLength = ThirdWord4.length;
+let FourthWordLength = FourthWord4.length;
+let FifthWordLength = FifthWord4.length;
+
+
+console.log('First Word: ' + FirstWord4 + ', with length: ' + firstWordLength);
+console.log('First Word: ' + SecondtWord4 + ', with length: ' + SecondtWordLength);
+console.log('First Word: ' + ThirdWord4 + ', with length: ' + ThirdWordLength);
+console.log('First Word: ' + FourthWord4 + ', with length: ' + FourthWordLength);
+console.log('First Word: ' + FifthWord4 + ', with length: ' + FifthWordLength);
